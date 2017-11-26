825d1a3e392614cf5ad18df8ed51c30f9375ec7b
SafeFly <ast850328@gmail.com>
2017-11-26 17:40:13 +0800

    api.js

---
+++ api.js
@@ -0,0 +1,66 @@
+const API = (function () {
+    const baseUrl = env.baseUrl;
+  
+    // 審核單位登入
+    function login(username, password) {
+      const data = {username, password};
+  
+      const request = fetch(`${baseUrl}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+        credentials: 'include'
+      });
+  
+      return _requestHandle(request);
+    }
+  
+    // 審核單位登入
+    function checkOrder(hex) {
+        const data = {"hex": hex};
+        const request = fetch(`${baseUrl}/user/admin`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials: 'include',
+          body: JSON.stringify(data)
+        });
+    
+        return _requestHandle(request);
+      }
+
+    // http request 的中介處理
+    function _requestHandle(request) {
+      return request.then(fetchResponse => {
+        return fetchResponse.json().then(data => {
+          return {
+            ok: fetchResponse.ok,
+            data,
+            statusCode: fetchResponse.status
+          };
+        }).then(response => {
+          // 錯誤時的處理
+  
+          // 沒錯閃邊去
+          if (response.ok) {
+            return response;
+          }
+  
+          // 設定兩種錯誤類型（單行 string 跟原始 string array）
+        //   response.singleErrorMessage = response.data.messages.join(',');
+        //   response.errorMessages = response.data.messages;
+  
+          return response;
+        });
+      })
+    }
+  
+    return {
+      login,
+      checkOrder
+    }
+  })();
+  
