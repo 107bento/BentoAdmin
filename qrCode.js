c060f5c518c4f7af15eb8f2ea917394d35f342a2
SafeFly <ast850328@gmail.com>
2017-12-12 18:30:43 +0800

    改一下 js，新增拿到訂單的功能，還有 Button，都是假的

--- qrCode.js
+++ qrCode.js
@@ -4,6 +4,9 @@ const app = (function () {
        * cache DOM
        */
     
+      let $detailsBody = $('#detailsBody');
+      let $orderID = $('#orderID');
+      let $receiveBtn = $('#receiveBtn');
       /**
        * init
        */
@@ -18,21 +21,37 @@ const app = (function () {
         
       }
       
-      function recieveOrder(hex) {
-        API.checkOrder(hex).then(response => {
+      function getOrder(hex) {
+        API.getOrder(hex).then(response => {
             if (response.statusCode == 200) {
-                
-                alert(response.data.success);
+                alert('GET!');
+                console.log(response.data);
+                $receiveBtn.prop('hidden', false);
+                _renderData(response.data);
               } else {
                 // 彈出錯誤訊息
                 alert(response.data.error);
               }
         });
       }
-      
-    
+
+      function _renderData(data) {
+        $orderID.val(data.order_id);
+        let html = ``;
+        for (let detail of data.details) {
+          html += `<tr><td>${detail.detail_id}</td><td>${detail.meal.shop_name}</td><td>${detail.meal.meal_name}</</td><td>${detail.amount}</td>`
+        }
+        $detailsBody.html(html);
+      }
+
+      function receiveOrder() {
+        alert('領餐成功！');
+        $receiveBtn.prop('disabled', true);
+      }
+
       return {
-        recieveOrder
+        getOrder,
+        receiveOrder
       }
     
     })();
